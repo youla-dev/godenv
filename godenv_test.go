98a2634e16477faa72ecaa108ec8de1e6f0b79d9
Ilya Sevostyanov <d3th@zeen.ru>
2022-02-24 19:24:31 +0300

    feat: The first version of the exporter.

---
+++ godenv_test.go
@@ -0,0 +1,32 @@
+package godenv_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/youla-dev/godenv"
+)
+
+func TestParse(t *testing.T) {
+	raw := `
+HTTP_ADDR=:80
+JAEGER_AGENT_ENDPOINT="jaeger-otlp-agent:6831"
+LOG_FORMAT='json'
+LOG_LEVEL=debug
+WAIT_BEFORE_EXIT=0s`
+
+	expected := map[string]string{
+		"HTTP_ADDR":             ":80",
+		"JAEGER_AGENT_ENDPOINT": "jaeger-otlp-agent:6831",
+		"LOG_FORMAT":            "json",
+		"LOG_LEVEL":             "debug",
+		"WAIT_BEFORE_EXIT":      "0s",
+	}
+
+	input := bytes.NewBufferString(raw)
+	values, err := godenv.Parse(input)
+	require.NoError(t, err)
+	assert.Equal(t, expected, values)
+}
